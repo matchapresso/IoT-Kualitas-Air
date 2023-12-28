42b446337ba6b34a6e545bb4631822e31fd4589d
Areta Vahtsa <102155545+dreamofu@users.noreply.github.com>
2023-12-28 19:22:24 +0700

    Create Blynk_1.1.h
    Another configuration for Blynk integration

---
+++ Blynk_1.1.h
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <DFRobot_PH.h>
+#include <GravityTDS.h>
+#include <GravityDO.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// Blynk credentials
+char auth[] = "Your_Blynk_Auth_Token";
+char ssid[] = "Your_WiFi_SSID";
+char pass[] = "Your_WiFi_Password";
+
+//main.h
