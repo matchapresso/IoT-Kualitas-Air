cd133fddeb5905ebfd63bf2a08eba3339dcfc3bf
Areta Vahtsa <102155545+dreamofu@users.noreply.github.com>
2024-02-04 19:37:57 +0700

    Update Blynk-main.h
    updated for Blynk connection and DS18B20

--- Blynk-main.h
+++ Blynk-main.h
@@ -1,26 +1,41 @@
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <DFRobot_PH.h>
-#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
 
-char auth[] = "YourAuthToken";  // Replace with your Blynk Auth Token
+char ssid[] = "POCO F4";
+char pass[] = "12345678";
 
-// ... (rest of the includes and sensor configurations remain unchanged)
+#define BLYNK_TEMPLATE_ID "TMPL6XYWmUcN3"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "H2Mfw1W--nUrvyYCC3dfAxGrtwyg76HT"
+
+#define ONE_WIRE_BUS 14
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float Celsius = 0;
+float Fahrenheit = 0;
+
+BlynkTimer timer;
+
+void temp() {
+  sensors.requestTemperatures();
+  Celsius = sensors.getTempCByIndex(0);
+  Fahrenheit = sensors.toFahrenheit(Celsius);
+  Blynk.virtualWrite(V1, Celsius);
+}
 
 void setup() {
   Serial.begin(115200);
-  mySerial.begin(9600);
-
-  // Blynk Setup
-  Blynk.begin(auth, "YourWiFiSSID", "YourWiFiPassword");
-
-  // ... (rest of the setup code remains unchanged)
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(1000L, temp);
 }
 
 void loop() {
   Blynk.run();
-
-  // ... (rest of the loop code remains unchanged)
+  timer.run();
 }
